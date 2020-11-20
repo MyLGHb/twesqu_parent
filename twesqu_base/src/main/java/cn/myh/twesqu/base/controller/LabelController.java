0321fa1a65abc77330c01969e6eae7f106e0ae73
myihao <myihao@linewell.com>
2020-11-20 18:41:39 +0800

    supplement

--- twesqu_base/src/main/java/cn/myh/twesqu/base/controller/LabelController.java
+++ twesqu_base/src/main/java/cn/myh/twesqu/base/controller/LabelController.java
@@ -1,12 +1,11 @@
 package cn.myh.twesqu.base.controller;
 
+import cn.myh.twesqu.base.pojo.Label;
 import cn.myh.twesqu.base.service.LabelService;
 import cn.myh.twesqu.common.entity.Result;
 import cn.myh.twesqu.common.entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +23,29 @@ public class LabelController {
     public Result findAll() {
         return new Result(true, StatusCode.OK,"查询成功",labelService.findAll());
     }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Result findById(@PathVariable String id) {
+        return new Result(true,StatusCode.OK,"查询成功",labelService.findById(id));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(Label label) {
+        labelService.addOrUpdate(label);
+        return new Result(true,StatusCode.OK,"增加成功");
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public Result update(@RequestBody Label label,@PathVariable String id) {
+        label.setId(id);
+        labelService.addOrUpdate(label);
+        return new Result(true,StatusCode.OK,"修改成功");
+    }
+
+    @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String id) {
+        labelService.deleteById(id);
+        return new Result(true,StatusCode.OK,"删除成功");
+    }
+
 }
