f44e5dfd9f8b48aa6cf7e598314f493d21b43a2b
myihao <myihao@linewell.com>
2020-12-28 14:53:44 +0800

    微服务注册，修改elasticsearch配置

--- twesqu_base/src/main/java/cn/myh/twesqu/base/BaseApplication.java
+++ twesqu_base/src/main/java/cn/myh/twesqu/base/BaseApplication.java
@@ -3,9 +3,11 @@ package cn.myh.twesqu.base;
 import cn.myh.twesqu.common.util.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class);
--- twesqu_search/src/main/java/cn/myh/twesqu/SearchApplication.java
+++ twesqu_search/src/main/java/cn/myh/twesqu/SearchApplication.java
@@ -2,9 +2,11 @@ package cn.myh.twesqu;
 import cn.myh.twesqu.common.util.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SearchApplication {
 
 	public static void main(String[] args) {
