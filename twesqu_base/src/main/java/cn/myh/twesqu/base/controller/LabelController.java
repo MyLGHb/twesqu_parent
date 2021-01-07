ffe69480641daf11074216c32e8299300bc39f45
myihao <myihao@linewell.com>
2021-01-07 17:52:47 +0800

    引入spring cloud config 和 spring cloud bus

--- twesqu_base/src/main/java/cn/myh/twesqu/base/controller/LabelController.java
+++ twesqu_base/src/main/java/cn/myh/twesqu/base/controller/LabelController.java
@@ -5,6 +5,8 @@ import cn.myh.twesqu.base.service.LabelService;
 import cn.myh.twesqu.common.entity.Result;
 import cn.myh.twesqu.common.entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,12 +15,16 @@ import java.util.Map;
 /**
  * 标签控制层
  */
+@RefreshScope
 @RestController
 @RequestMapping("/label")
 public class LabelController {
 
     @Autowired
     private LabelService labelService;
+
+    @Value("${mess}")
+    private String mess;
 
     @RequestMapping(method = RequestMethod.GET)
     public Result findAll() {
@@ -59,6 +65,11 @@ public class LabelController {
                                     @PathVariable int page, @PathVariable int size) {
         return new Result(true,StatusCode.OK,"查询成功",
                 labelService.searchByCondition(searchMap,page,size));
+    }
+
+    @RequestMapping(value = "/springCloudBusTest")
+    public String springCloudBusTest() {
+        return mess;
     }
 
 }
