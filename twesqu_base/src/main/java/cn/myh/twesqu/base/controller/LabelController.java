07f77f6889cf35444b22dfa9950556eb94f18e87
myihao <myihao@linewell.com>
2021-01-25 18:19:36 +0800

    添加dockerfile自动化配置，未实现

--- twesqu_base/src/main/java/cn/myh/twesqu/base/controller/LabelController.java
+++ twesqu_base/src/main/java/cn/myh/twesqu/base/controller/LabelController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 /**
